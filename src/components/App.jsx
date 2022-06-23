8d9f2f567dd5980bc4a08133f7392052359f8b25
fulim <fulim1988@gmail.com>
2022-06-23 09:59:41 +0800

    Add App.jsx, Header.jsx, Note.jsx & Footer.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// components
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+
+}
+
+export default App;
