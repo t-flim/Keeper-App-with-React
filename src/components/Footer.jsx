e646b28a5ae8d815c8b09786018150be2d1217a6
fulim <fulim1988@gmail.com>
2022-06-23 10:08:03 +0800

    Add dynamic year display

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Footer() {
-    
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
+    return (
+        <footer>
+            <p>
+                Copyright &copy; {currentYear}
+            </p>
+        </footer>
+    )
 }
 
 export default Footer;
