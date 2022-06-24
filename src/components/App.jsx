6da665eb98827c54ae4fb603ac418e4d2dbfb67c
fulim <fulim1988@gmail.com>
2022-06-24 11:24:57 +0800

    Import notes.jx, Map notes array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,19 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map(note => (
+                <Note 
+                    key={note.key}
+                    name={note.name}
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     )
