e45c0f1e4a129257be0426f129967270f5fcde04
fulim <fulim1988@gmail.com>
2022-06-24 11:25:57 +0800

    Add props to Notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
     return (
         <div className="note">
             <h1>
-                This is the note title
+                {props.name}
             </h1>
             <p>
-                This is the note content
+                {props.content}
             </p>
         </div>
     )
